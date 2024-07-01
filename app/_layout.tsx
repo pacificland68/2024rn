0ca3da41c4893befa5544857dc0a8c051ee851ee
Jack Aubrey <pacificland68@hotmail.com>
2024-07-01 22:31:32 +0800

    711

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,7 @@ import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
+import { StyleSheet, Text, View } from 'react-native'
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
@@ -13,7 +14,7 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require('../assets/fonts/Poppins-Regular.ttf'),
   });
 
   useEffect(() => {
@@ -27,11 +28,17 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }}/>
+      <Stack.Screen name="+not-found" />
+    </Stack>
   );
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
---
+++ app/index.jsx
@@ -0,0 +1,54 @@
+import { StatusBar } from 'expo-status-bar'
+import { Text, View, ScrollView, Image } from 'react-native'
+import { Redirect, router } from 'expo-router'
+import {SafeAreaView} from 'react-native-safe-area-context'
+import CustomButton from '../components/CustomButton'
+
+import { images } from '../constants'
+
+export default function App(){
+    return (
+        <SafeAreaView className="bg-primary h-full">
+            <ScrollView contentContainerStyle={{height: '100%'}}>
+                <View className="w-full justify-center items-center h-full px-4">
+                    <Image 
+                        source={images.logo}
+                        className="w-[130px] h-[84px]"
+                        resizeMode="contain"
+                    />
+
+                    <Image 
+                        source={images.cards}
+                        className="max-w--[380px] w-full h-[300px]"
+                        resizeMode="contain"
+                    />
+
+                    <View className="relative mt-5">
+                        <Text className="text-3xl text-white font-bold text-center">
+                            Discover Endless Possibilities with{' '}
+                            <Text className="text-secondary-200">Frank</Text>
+                        </Text>
+
+                        <Image 
+                            source={images.path}
+                            className="w-136px h-[15px] absolute -bottom-2 -right-12"
+                            resizeMode="contain"
+                        />
+                    </View>
+
+                    <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+                        Where creativity meets innovation: embark on a jorney of limitless exploration with Frank
+                    </Text>
+
+                    <CustomButton 
+                        title="Continue with Email"
+                        handlePress={() => router.push('/sign-in')}
+                        containerStyles="w-full mt-7"
+                    />
+                </View>
+            </ScrollView>
+
+            <StatusBar backgroundColor="#161622" style="light"/>
+        </SafeAreaView>
+    )
+}
